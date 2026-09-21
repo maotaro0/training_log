9c59763d9ebd49b320a4eccbd45da58fbe6f48bc
maotaro0 <ma.prs.10@gmail.com>
2026-09-21 18:17:47 +0900

    トレーニング記録の編集等

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,3 +1,10 @@
 class TrainingRecord < ApplicationRecord
   belongs_to :user
+
+  validates :training_day, presence: true
+  validates :training_name, presence: true
+  validates :weight_kg, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :reps, numericality: { only_integer: true, greater_than: 0 }
+  validates :set_count, numericality: { only_integer: true, greater_than: 0 }
+
 end
