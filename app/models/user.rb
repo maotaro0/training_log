245ca7bb68453429c949d5b0e0e6d5eebcf95f7e
maotaro0 <ma.prs.10@gmail.com>
2026-09-23 02:16:48 +0900

    月間回数

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
          :validatable
 
   has_many :training_records
+
+  def monthly_training_count
+     training_records
+      .where(training_day: Date.current.beginning_of_month..Date.current.end_of_month)
+      .distinct
+      .count(:training_day)
+   end
 end
